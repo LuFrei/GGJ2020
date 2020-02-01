e3b5c9e057fe25dbf483eecb207b511cd7cb88c6
Lucas Freitas <lucasfreitas09876@gmail.com>
2020-01-31 20:00:31 -0500

    Character Moves
    Woohoo!

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Player
+{
+
+    private void Update(){
+        Move(GetMoveInput());
+    }
+
+    Vector2 GetMoveInput(){
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        return new Vector2(horizontal, vertical);
+    }
+
+    void Move(Vector2 moveVector){
+        transform.Translate(moveVector * Time.deltaTime);
+    }
+
+
+}
