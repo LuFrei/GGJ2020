d91612d8449e353d34faf362b150d128276172b6
LeafeonZ <Olivia_zuern@bloomfield.edu>
2020-02-01 13:31:02 -0500

    Rotation, started menu UI handling

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+	public List<GameObject> buttons;
+
+	private int index = 0; // index of selected button
+	private GameObject selector;
+	private bool moveSelector = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per fram
+	void Click() {
+		// color
+		buttons[index].GetComponent<Image>().CrossFadeColor(Color.gray, 0.25f, true, true);
+
+		// operation
+		switch (index) {
+			case 1:
+				break;
+			case 2:
+				break;
+
+		}
+
+
+	}
+	void Update() {
+		if ((int)Input.GetAxis("Vertical") !=0) {
+			index += (int)Input.GetAxis("Vertical");
+			moveSelector = true;
+		}
+		
+		
+		// if selector not on button, move towards it.
+		if (moveSelector) {
+			// move towards
+			selector.transform.position = Vector3.MoveTowards(selector.transform.position, buttons[index].transform.position, 10 * Time.deltaTime);
+			if(Vector3.Distance(selector.transform.position, buttons[index].transform.position) < 0.01f) {
+				selector.transform.position = buttons[index].transform.position;
+				moveSelector = false;
+			}
+		}
+
+		//on A pressed, --> click()
+		if (Input.GetButtonDown("a")) {
+			Click();
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : Player
 {
+	
 
     private void Update(){
         Move(GetMoveInput());
@@ -16,9 +17,21 @@ public class PlayerController : Player
         return new Vector2(horizontal, vertical);
     }
 
-    void Move(Vector2 moveVector){
-        transform.Translate(moveVector * Time.deltaTime);
-    }
+	void Move(Vector2 moveVector) {
+		//transform.Translate(moveVector * Time.deltaTime);
+		transform.position = transform.position + (new Vector3 (moveVector.x,moveVector.y,0) * Time.deltaTime);
 
+		float angle = 0;
+		if (Mathf.Abs(moveVector.x) > Mathf.Abs(moveVector.y)) {
+			// use X-
+			if (moveVector.x > 0) { angle = 0; } else { angle = 180; }
+		} else {
+			if (moveVector.y > 0) { angle = 90; } else { angle = 270; }
+		}
+		transform.rotation = Quaternion.Euler(0, 0, angle);
+		}
+		
 
 }
+
+
