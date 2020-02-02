929e73a6cfc0ec4a72a4ec12acbb7e223c38138e
LeafeonZ <Olivia_zuern@bloomfield.edu>
2020-02-02 14:19:47 -0500

    MenuCommit

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,56 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
 	public List<GameObject> buttons;
+	public List<Sprite> select;
+	public List<Sprite> press;
+	private List<Sprite> norm;
 
+	public bool isCreds = false;
+	public GameObject creds;
 	private int index = 0; // index of selected button
+	private bool menuSwitch = false;
 	private GameObject selector;
-	private bool moveSelector = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+		norm = new List<Sprite>();
+		for(int i = 0; i<buttons.Count; i++) { norm.Add(buttons[i].GetComponent<Image>().sprite); }
     }
 
-    // Update is called once per fram
+	// Update is called once per fram
+	void Transition() { SceneManager.LoadScene("BattleScene1"); }
+	void Quit() { Application.Quit(); }
 	void Click() {
 		// color
-		buttons[index].GetComponent<Image>().CrossFadeColor(Color.gray, 0.25f, true, true);
+
 
 		// operation
 		switch (index) {
+			case 0:
+				Invoke("Transition", 0.25f);
+				break;
+
 			case 1:
+				isCreds = true;
+				creds.SetActive(true);
 				break;
 			case 2:
+				Invoke("Quit", 0.2f);
 				break;
 
 		}
 
 
 	}
+	void MenuSwitch() { menuSwitch = false; }
 	void Update() {
-		if ((int)Input.GetAxis("Vertical") !=0) {
-			index += (int)Input.GetAxis("Vertical");
-			moveSelector = true;
-		}
-		
-		
-		// if selector not on button, move towards it.
-		if (moveSelector) {
-			// move towards
-			selector.transform.position = Vector3.MoveTowards(selector.transform.position, buttons[index].transform.position, 10 * Time.deltaTime);
-			if(Vector3.Distance(selector.transform.position, buttons[index].transform.position) < 0.01f) {
-				selector.transform.position = buttons[index].transform.position;
-				moveSelector = false;
+		if (!isCreds && !menuSwitch) {
+			if ((int)Input.GetAxis("P1Vert") != 0) {
+				buttons[index].GetComponent<Image>().sprite = norm[index];
+				index += (int)Input.GetAxis("P1Vert");
+				if (index > 2) { index = 0; }
+				if (index < 0) { index = 2; }
+				buttons[index].GetComponent<Image>().sprite = select[index];
+				menuSwitch = true;
+				Invoke("MenuSwitch", 0.5f);
+			}
+
+
+			// if selector not on button, move towards it.
+
+
+			//on A pressed, --> click()
+			if (!Input.GetButton("P1A")) { buttons[index].GetComponent<Image>().sprite = select[index]; }
+			if (Input.GetButtonDown("P1A")) {
+				buttons[index].GetComponent<Image>().sprite = press[index];
+				Click();
+			}
+		} else {
+			if (Input.GetButtonDown("P1A")) {
+				isCreds = false;
+				creds.SetActive(false);
 			}
 		}
 
-		//on A pressed, --> click()
-		if (Input.GetButtonDown("a")) {
-			Click();
-		}
 	}
 }
