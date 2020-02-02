8fdb090ab66336fc68eec746152715bd2c082ceb
Lucas Freitas <lucasfreitas09876@gmail.com>
2020-02-01 19:28:44 -0500

    Aiming Needs to be Tested

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private PlayerController pControl;
+
+    void Start(){
+        pControl = GetComponentInParent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetPlayerAngle();
+    }
+
+    void SetPlayerAngle(){
+        float angle = 0;
+
+        Vector2 lookVector;
+
+
+        //If the player is "Aiming" use Aim values
+        if (pControl.GetAimAngle() != Vector2.zero){
+            lookVector = pControl.GetAimAngle();
+        } else{
+            lookVector = pControl.GetMoveVector();
+        }
+
+
+        if (Mathf.Abs(lookVector.x) > Mathf.Abs(lookVector.y))
+        {
+            // use X-
+            if (lookVector.x > 0) { angle = 0; } else { angle = 180; }
+        }
+        else
+        {
+            if (lookVector.y > 0) { angle = 90; } else { angle = 270; }
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,32 +7,30 @@ public class PlayerController : Player
 	
     private void Update(){
         Move(GetMoveVector());
+
+
+		Debug.Log($"Aim Angle: {GetAimAngle()}");
 	}
 
-    Vector2 GetMoveVector(){
+    public Vector2 GetMoveVector(){
         float horizontal = Input.GetAxis("P" + index + "Hoz") * speed * Time.deltaTime;
         float vertical = Input.GetAxis("P" + index + "Vert") * speed * Time.deltaTime;
 
         return new Vector2(horizontal, vertical);
     }
 
+	public Vector2 GetAimAngle()
+	{
+		float horizontal = Input.GetAxis("P" + index + "AimHoz");
+		float vertical = Input.GetAxis("P" + index + "AimVert");
 
+		return new Vector2(horizontal, vertical);
+	}
 
 	void Move(Vector2 moveVector) {
 		//transform.Translate(moveVector * Time.deltaTime);
 		transform.position += (new Vector3 (moveVector.x,moveVector.y,0) * Time.deltaTime);
-
-		float angle = 0;
-		if (Mathf.Abs(moveVector.x) > Mathf.Abs(moveVector.y)) {
-			// use X-
-			if (moveVector.x > 0) { angle = 0; } else { angle = 180; }
-		} else {
-			if (moveVector.y > 0) { angle = 90; } else { angle = 270; }
-		}
-		transform.rotation = Quaternion.Euler(0, 0, angle);
-		}
-		
-
+	}
 }
 
 
