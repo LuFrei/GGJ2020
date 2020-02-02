19c58567dd5999adbf8905da550a4884f33055df
LeafeonZ <Olivia_zuern@bloomfield.edu>
2020-02-02 10:17:25 -0500

    Art Add 1
    Added art assets, set up player anim and rotation

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -6,39 +6,50 @@ public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private PlayerController pControl;
 
+	private float baseSpeed;
+	private Animator anim;
     void Start(){
         pControl = GetComponentInParent<PlayerController>();
+		baseSpeed = pControl.speed;
+		anim = gameObject.GetComponentInParent<Animator>();
+		anim.SetBool("Team", pControl.team);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        SetPlayerAngle();
-    }
+	// Update is called once per frame
+	void Update() {
+		SetPlayerAngle();
 
-    void SetPlayerAngle(){
-        float angle = 0;
+		// glue = .2 multiplier;
+		float move = (pControl.speed / baseSpeed) * pControl.GetMoveVector().normalized.magnitude;
+		anim.SetFloat("Speed", move);
+		if (pControl.GetMoveVector().magnitude <= 0.1f) {
+			anim.SetBool("IsWalk", false);
+		} else { anim.SetBool("IsWalk", true); }
+	}
 
-        Vector2 lookVector;
+	void SetPlayerAngle() {
+		float angle = 0;
 
-
-        //If the player is "Aiming" use Aim values
-        if (pControl.GetAimAngle() != Vector2.zero){
-            lookVector = pControl.GetAimAngle();
-        } else{
-            lookVector = pControl.GetMoveVector();
-        }
+		Vector2 lookVector;
 
 
-        if (Mathf.Abs(lookVector.x) > Mathf.Abs(lookVector.y))
-        {
-            // use X-
-            if (lookVector.x > 0) { angle = 0; } else { angle = 180; }
-        }
-        else
-        {
-            if (lookVector.y > 0) { angle = 90; } else { angle = 270; }
-        }
-        transform.rotation = Quaternion.Euler(0, 0, angle);
-    }
+		//If the player is "Aiming" use Aim values
+		if (pControl.GetAimAngle() != Vector2.zero) {
+			lookVector = pControl.GetAimAngle();
+
+		} else {
+			lookVector = pControl.GetMoveVector();
+			
+		}
+		lookVector = new Vector2(-lookVector.y, lookVector.x);
+		if (lookVector != Vector2.zero) {
+			if (Mathf.Abs(lookVector.x) > Mathf.Abs(lookVector.y)) {
+				// use X-
+				if (lookVector.x > 0) { angle = 0; } else { angle = 180; }
+			} else {
+				if (lookVector.y > 0) { angle = 90; } else { angle = 270; }
+			}
+			transform.rotation = Quaternion.Euler(0, 0, angle);
+		}
+	}
 }
