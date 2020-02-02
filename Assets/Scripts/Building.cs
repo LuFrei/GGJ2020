99ca650374cc676febeda5b8524f1acb40cd4524
LeafeonZ <Olivia_zuern@bloomfield.edu>
2020-02-02 14:42:15 -0500

    Backer added, building anim set.

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Building : MonoBehaviour{
-    public int pointValue; //from 0-100
+public class Building : MonoBehaviour {
+	public int pointValue; //from 0-100
+	Animator anim;
+
+	private void Start() {
+		anim = gameObject.GetComponent<Animator>();
+
+	}
+	//anim.SetFloat("Progress",pointValue/1000);
+
 }
